b7e3d35168cdd5d2e655ccf968cd7a9c87b7a597
celeste95 <qduzhaopu@gmail.com>
2017-11-13 11:58:11 +0800

    171112 both recursion and iteration

--- 101-200/101-Symmetric Tree.cpp
+++ 101-200/101-Symmetric Tree.cpp
@@ -10,7 +10,7 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        //my iteration solution
+        //my recursion solution
         if(root==NULL) return true;
         if(!(root->left||root->right)) return true;
         if((root->left==NULL)^(root->right==NULL)) return false;
@@ -32,7 +32,7 @@ public:
 
 };
 
-//example iteration solution
+//example recursion solution
 class Solution {
     bool isSymmetric(TreeNode *node1, TreeNode *node2) {
         if (node1 == nullptr && node2 == nullptr) return true;
@@ -48,15 +48,50 @@ public:
     }
 };
 
+
+//example iteration solution
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL) return true;
-        if(!(root->left||root->right)) return true;
-        if((root->left==NULL)^(root->right==NULL)) return false;
-        if(root->left->val!=root->right->val) return false;
+        if(root == nullptr) return true;
+        queue<TreeNode *> q;
+        q.push(root->left);
+        q.push(root->right);
+        while (!q.empty()) {
+            /* code */
+            TreeNode *l=q.front();
+            q.pop();
+            TreeNode *r=q.front();
+            q.pop();
+            if (l == nullptr && r == nullptr)  continue;
+            else 
+            {
+                if (l==nullptr||r==nullptr) {
+                    return false;
+                }
+                else
+                {
+                    if(l->val!=r->val) return false;
+                    q.push(l->left);
+                    q.push(r->right);
+                    q.push(l->right);
+                    q.push(r->left);
+                    
+                }
+            }
+        }
+        return true;
     }
 };
 
 
-//
+
+
+
+
+
+
+
+
+
+
