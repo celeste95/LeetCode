9ff78e9e35447b741489d82313a107b8905a1367
celeste95 <qduzhaopu@gmail.com>
2017-11-22 14:58:55 +0800

    null

---
+++ 101-200/190-Reverse Bits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+		//my solution
+        uint32_t ret=0;
+        for(int i=0;i<32;i++)
+        {
+            ret=ret*2+n%2;
+            n=n/2;
+        }
+        return ret;
+    }
+};
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+		//example solution
+        n = (n<<16) | (n>>16);
+        n = (((n & 0xFF00FF00)>>8) | ((n & 0x00FF00FF)<<8));
+        n = (((n & 0xF0F0F0F0)>>4) | ((n & 0x0F0F0F0F)<<4));
+        n = (((n & 0xCCCCCCCC)>>2) | ((n & 0x33333333)<<2));
+        n = (((n & 0xAAAAAAAA)>>1) | ((n & 0x55555555)<<1));
+        return n;
+    }
+};
