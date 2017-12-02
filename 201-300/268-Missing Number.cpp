bb9444ed8c8f112991bd4af4de66fe73f2183fc3
celeste95 <qduzhaopu@gmail.com>
2017-12-02 21:28:23 +0800

    Binary Search XOR SUM

---
+++ 201-300/268-Missing Number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++) if (nums[i]!=i) return i;
+        return nums.size();
+    }
+};
+
+//1.XOR
+public int missingNumber(int[] nums) { //xor
+    int res = nums.length;
+    for(int i=0; i<nums.length; i++){
+        res ^= i;
+        res ^= nums[i];
+    }
+    return res;
+}
+//2.SUM
+public int missingNumber(int[] nums) { //sum
+    int len = nums.length;
+    int sum = (0+len)*(len+1)/2;
+    for(int i=0; i<len; i++)
+        sum-=nums[i];
+    return sum;
+}
+//3.Binary Search
+public int missingNumber(int[] nums) { //binary search
+    Arrays.sort(nums);
+    int left = 0, right = nums.length, mid= (left + right)/2;
+    while(left<right){
+        mid = (left + right)/2;
+        if(nums[mid]>mid) right = mid;
+        else left = mid+1;
+    }
+    return left;
+}
