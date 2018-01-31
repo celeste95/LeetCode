a40a5151d92c5f5c3c5913ee84993832dd257877
Zhao Pu <33062636+celeste95@users.noreply.github.com>
2018-01-31 16:12:36 +0800

    walker and runner ~~~!!!

---
+++ 101-200/142-Linked List Cycle II.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==nullptr) return nullptr;
+        ListNode *walker = head;
+        ListNode *runner = head;
+        int cnt=0;
+        while(runner->next!=nullptr && runner->next->next!=nullptr) {
+            walker = walker->next;
+            runner = runner->next->next;
+            cnt++;
+            if(walker==runner) {
+                walker = head;
+                while(walker!=runner) {
+                    walker=walker->next;
+                    runner=runner->next;
+                }
+                return walker;
+            }
+        }
+        return nullptr;
+    }
+};
