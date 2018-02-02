6e010a78e0df12dffa881d27ce3804a3cff1e2ff
Zhao Pu <33062636+celeste95@users.noreply.github.com>
2018-02-02 19:58:34 +0800

    Create 133-Clone Graph.cpp

---
+++ 101-200/133-Clone Graph.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if (!node) return NULL;
+        if (mp.find(node) == mp.end()) {
+            mp[node] = new UndirectedGraphNode(node -> label);
+            for (UndirectedGraphNode* neigh : node -> neighbors)
+                mp[node] -> neighbors.push_back(cloneGraph(neigh));
+        }
+        return mp[node];
+    } 
+private:
+    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> mp;
+};
